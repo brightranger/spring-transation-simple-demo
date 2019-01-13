af5deca013672705da009f582e48fb084e98802e
brightranger <332478519@qq.com>
2019-01-13 15:12:01 +0800

    提交手动事务代码

---
+++ src/test/java/com/zhanghf/service/impl/TransactionTemplateServiceTest.java
@@ -0,0 +1,18 @@
+package com.zhanghf.service.impl;
+
+import com.zhanghf.entry.Blog;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TransactionTemplateServiceTest {
+
+    @Autowired
+    TransactionTemplateService transactionTemplateService;
+
+    @Test
+    public void save(){
+        Blog blog = new Blog(18,"lili","url");
+        transactionTemplateService.save(blog);
+
+    }
+}
